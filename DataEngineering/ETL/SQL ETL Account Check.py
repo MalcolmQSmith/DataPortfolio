9c09fa0ff864bb55d0679d8c36fa70334cb8d3f8
MalcolmQSmith <121270158+MalcolmQSmith@users.noreply.github.com>
2026-04-27 22:43:24 -0400

    Add files via upload

---
+++ DataEngineering/ETL/SQL ETL Account Check.py
@@ -0,0 +1,64 @@
+import os
+import psycopg2
+from psycopg2 import sql
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_CONFIG = {
+    "host": os.getenv("DB_HOST"),
+    "port": 5432,
+    "dbname": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+}
+
+SCHEMA = "analytics"
+SOURCE_TABLE = "source_actuals"
+
+TABLE_CONFIGS = [
+    {
+        "name": "GL Accounts",
+        "target_table": "gl_account_hierarchy",
+        "source_key": "account_key",
+        "primary_key": "gl_account"
+    },
+    {
+        "name": "Cost Centers",
+        "target_table": "cost_center_hierarchy",
+        "source_key": "cost_center_key",
+        "primary_key": "cost_center_key"
+    }
+]
+
+
+def insert_new_records(cursor, cfg):
+    query = sql.SQL("""
+        INSERT INTO {schema}.{target} ({pk})
+        SELECT DISTINCT s.{source_key}
+        FROM {schema}.{source} s
+        LEFT JOIN {schema}.{target} t
+            ON s.{source_key} = t.{pk}
+        WHERE t.{pk} IS NULL
+        ON CONFLICT DO NOTHING;
+    """).format(
+        schema=sql.Identifier(SCHEMA),
+        source=sql.Identifier(SOURCE_TABLE),
+        target=sql.Identifier(cfg["target_table"]),
+        source_key=sql.Identifier(cfg["source_key"]),
+        pk=sql.Identifier(cfg["primary_key"]),
+    )
+
+    cursor.execute(query)
+
+
+def run():
+    with psycopg2.connect(**DB_CONFIG) as conn:
+        with conn.cursor() as cursor:
+            for cfg in TABLE_CONFIGS:
+                insert_new_records(cursor, cfg)
+        conn.commit()
+
+
+if __name__ == "__main__":
+    run()
